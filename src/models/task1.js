ed5cf48578237183c97806927d68d48ed3f09ea6
Alvaro Willams Alejandro Arancibia <aljand321@gmail.com>
2019-02-11 14:51:30 -0400

    captcha

--- src/models/task1.js
+++ src/models/task1.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const userP = Schema ({
+const userP1 = Schema ({
   name: String,
   price: String,
   description: String
 });
 
-module.exports = mongoose.model('task', userP);
+module.exports = mongoose.model('task1', userP1);
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,8 @@ const fetch = require('node-fetch');
 const request = require('request');
 
 const REgUSER = require('../models/regUser');
+const TASK = require('../models/task');
+const TASK1 = require('../models/task1');
 
 router.get('/', (req, res) => {
   res.render('index');
@@ -14,72 +16,103 @@ router.get('/regUSer', (req, res) => {
   res.render('regUSer');
 });
 
-//servicio para guardar en REgUSER
-// router.post('/addUser1', async (req, res) => {
-//   const reguser = new regU(req.body);
-//   await reguser.save();
-//   console.log(reguser);
-//   res.render('regUSer');
-//
-// });
-//servicio para guardar en REgUSER
-router.post("/addUser", async(req, res) => {
-  var reg_user = {
-    nombre: req.body.nombre,
-    apellidoP: req.body.apellidoP,
-    apellidoM: req.body.apellidoM,
-    email: req.body.email,
-    clave: req.body.clave,
-    clave2: req.body.clave2,
-    celular: req.body.celular,
-    direccion: req.body.direccion
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<
 
-  };
-  var user = new REgUSER(reg_user);
-  if(reg_user.clave == reg_user.clave2){
-    console.log(reg_user.clave2, reg_user.clave);
-
-    REgUSER.find({email: req.body.email}).exec( async (err, docs) => {
-      console.log(docs);
-        if(docs != ""){
-          console.log('ya existe ese email')
-          res.status(400).json({
-                  "msn" : "ya existe ese email"
-                });
-          // res.send("ya existe ese email")
-        }
-        else{
-            if(reg_user.nombre == ""){
-              res.status(400).json({
-                'msn' : 'Introdusca un nombre'
-              });
-            }else
-            if(reg_user.apellidoP==""){
-              res.status(400).json({
-                'msn' : 'Introdusca su apellido paterno'
-              });
-            }else{
-              await user.save();
-              console.log('enviado');
-              res.status(200).json({
-                "msn": "enviado"
-              });
-            }
-
-            // res.send('se enviaron los datos')
-        }
-
-    });
-  }
-  else{
-    console.log('las claves no son iguales');
-    res.status(400).json({
-            "msn" : "claves diferentes"
-          });
-    //res.send('las claves no son iguales');
-    // res.render('regUSer');
-  }
+  router.post('/addUser2', async (req, res, err) => {
+    if(err){
+      res.status(400).json({
+        'msn' : err
+      });
+    }else{
+      const task1 = new TASK1(req.body);
+      await task1.save();
+      console.log(task1);
+      res.status(200).json({
+          'msn' : task1
+       });
+    }
 });
+//>>>>>>>>>>>ZZZ
+router.get("/getP1", async(req, res) => {
+  const ver = await TASK1.find();
+  res.status(200).json(ver);
+});
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//servicio para guardar en REgUSER
+router.post('/addUser1', async (req, res) => {
+  const task = new TASK(req.body);
+  await task.save();
+  console.log(task);
+  res.status(200).json({
+      'msn' : task
+   });
+
+});
+
+router.get("/getP", async(req, res) => {
+  const verU = await TASK.find();
+  res.status(200).json(verU);
+});
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+//servicio para guardar en REgUSER
+// router.post("/addUser", async(req, res) => {
+//   var reg_user = {
+//     nombre: req.body.nombre,
+//     apellidoP: req.body.apellidoP,
+//     apellidoM: req.body.apellidoM,
+//     email: req.body.email,
+//     clave: req.body.clave,
+//     clave2: req.body.clave2,
+//     celular: req.body.celular,
+//     direccion: req.body.direccion
+//
+//   };
+//   var user = new REgUSER(reg_user);
+//   if(reg_user.clave == reg_user.clave2){
+//     console.log(reg_user.clave2, reg_user.clave);
+//
+//     REgUSER.find({email: req.body.email}).exec( async (err, docs) => {
+//       console.log(docs);
+//         if(docs != ""){
+//           console.log('ya existe ese email')
+//           res.status(400).json({
+//                   "msn" : "ya existe ese email"
+//                 });
+//           // res.send("ya existe ese email")
+//         }
+//         else{
+//             if(reg_user.nombre == ""){
+//               res.status(400).json({
+//                 'msn' : 'Introdusca un nombre'
+//               });
+//             }else
+//             if(reg_user.apellidoP==""){
+//               res.status(400).json({
+//                 'msn' : 'Introdusca su apellido paterno'
+//               });
+//             }else{
+//               await user.save();
+//               console.log('enviado');
+//               res.status(200).json({
+//                 "msn": "enviado"
+//               });
+//             }
+//
+//             // res.send('se enviaron los datos')
+//         }
+//
+//     });
+//   }
+//   else{
+//     console.log('las claves no son iguales');
+//     res.status(400).json({
+//             "msn" : "claves diferentes"
+//           });
+//     //res.send('las claves no son iguales');
+//     // res.render('regUSer');
+//   }
+// });
 
 // servicio para mostrar todos usuarios
 router.get("/getUser", async(req, res) => {
@@ -135,8 +168,7 @@ router.post('/sessions', function (req, res) {
   });
 });
 
-//para reCAPTCHA
-
+//para reCAPTCHA y envio de datos
 router.post('/reCAPTCHA', async(req, res) => {
   var reg_user = {
     nombre: req.body.nombre,
@@ -187,14 +219,35 @@ router.post('/reCAPTCHA', async(req, res) => {
           else{
               if(reg_user.nombre == ""){
                 res.status(400).json({
-                  'msn' : 'Introdusca un nombre'
+                  'msn' : 'Introduzca un nombre'
                 });
               }else
               if(reg_user.apellidoP==""){
                 res.status(400).json({
-                  'msn' : 'Introdusca su apellido paterno'
+                  'msn' : 'Introduzca su apellido paterno'
                 });
-              }else{
+              }else
+                if(reg_user.clave.length < 6 ){
+                  res.status(400).json({
+                    'msn' : 'Las contraseñas deben tener al menos 6 caracteres'
+                  });
+
+                }else
+                if(reg_user.celular.length <7){
+                  res.status(400).json({
+                    'msn' : 'Introduzca un numero de celular válido mas de 7 digitos'
+                  });
+                }else
+                if(reg_user.email == ""){
+                  res.status(400).json({
+                    'msn' : 'La dirección de correo electrónico es obligatoria.'
+                  });
+                }else
+                if(isNaN(reg_user.celular)){
+                  res.status(400).json({
+                    'msn' : 'Introduzca un numero de celular válido.'
+                  });
+                }else{
                 await user.save();
                 console.log('enviado');
                 res.status(200).json({
